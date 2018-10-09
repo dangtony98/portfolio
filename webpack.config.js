f00fa51215c529546850b2da30ce0b2c1b54df0d
Tuan Dang <tuandang@MacBook-Pro-2.local>
2018-10-09 14:26:47 -0400

    Add image-webpack-loader for image compression

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,27 @@ module.exports = {
               {
                 loader: 'file-loader',
                 options: {}  
+              }, {
+                  loader: 'image-webpack-loader',
+                  options: {
+                    mozjpeg: {
+                      progressive: true,
+                      quality: 65
+                    },
+                    optipng: {
+                      enabled: false,
+                    },
+                    pngquant: {
+                      quality: '65-90',
+                      speed: 4
+                    },
+                    gifsicle: {
+                      interlaced: false,
+                    },
+                    webp: {
+                      quality: 75
+                    }
+                  }
               }
             ]
           }
